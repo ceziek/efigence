5af8560405538e172bc1803f72f2d6166daff6fd
ceziek <c.bajera@gmail.com>
2016-08-11 19:48:44 +0200

    added ajax response

--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 $(document).foundation();
+
+$(document).on('ready', function () {
+    $('.button-go').on('click', function () {
+
+        var pass = $("#password").val();
+        var loginNumber = $('#login').html();
+
+        var obj = {
+            login:  loginNumber,
+            password: pass
+        };
+
+        login(obj);
+    });
+
+    $('.avatar').on('click', function () {
+
+        var bg = $(this).css('background-image');
+        bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
+        alert(bg);
+    });
+
+
+
+    var login = function (obj) {
+        $.ajax({
+            type: "post",
+            data: obj,
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            error: function(response) {
+                console.log(response.responseText);
+            },
+            success: function(response) {
+                $('#response').html(JSON.stringify(response)).show();
+                console.log(response);
+            }
+        });
+    }
+});
