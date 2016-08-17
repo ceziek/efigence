9be480a58aa51ff2211fb0124c7b45fd5282528f
ceziek <c.bajera@gmail.com>
2016-08-18 01:38:08 +0200

    added input validation and new awesome features !

--- js/app.js
+++ js/app.js
@@ -1,40 +1,131 @@
 $(document).foundation();
 
 $(document).on('ready', function () {
-    $('.button-go').on('click', function () {
 
-        var pass = $("#password").val();
-        var loginNumber = $('#login').html();
+    /**
+     * jQuery function for display pop-up message
+     */
+    (function ($) {
+        $.fn.message = function (msg) {
+            $(this)
+                .finish()
+                .html(msg)
+                .fadeIn()
+                .delay(3000)
+                .fadeOut();
+        };
+    })(jQuery);
 
-        var obj = {
-            login:  loginNumber,
+
+    /**
+     * jQuery function for input validation
+     */
+
+    (function ($) {
+        $.fn.valid = function () {
+            var value = true;
+            this.msg = "Password must be";
+
+            if ($(this).val().length < 4) {
+                this.msg += " at least 4 characters";
+                value = false;
+            }
+
+            if ($(this).val().length > 10) {
+                this.msg += " not more than 10 characters";
+                value = false;
+            }
+
+            return value;
+        };
+    })(jQuery);
+
+
+    /**
+     *
+     * Ajax request for login
+     * @param {Object} credits:
+     * {
+     *  login: "",
+     *  password: ""
+     * }
+     *
+     */
+    var login = function (credits) {
+        $.ajax({
+            type: "post",
+            data: credits,
+            url: "https://efigence-camp.herokuapp.com/api/login",
+            beforeSend: function () {
+                loader.fadeIn();
+            },
+            error: function (res) {
+                var response = JSON.parse(res.responseText).message;
+
+                messageNode.message(response)
+            },
+            success: function (res) {
+                if (res.status == true) location.href = "http://www.wp.pl"
+            },
+            complete: function () {
+                loader.fadeOut();
+            }
+        });
+    };
+
+
+    /**
+     * Get login and password from form elements,
+     * and pass them to login() as {Object}
+     */
+    var submitLogin = function () {
+
+        var name = loginName.html();
+        var pass = passwordInput.val();
+
+        var credits = {
+            login: name,
             password: pass
         };
 
-        login(obj);
-    });
-
-    $('.avatar').on('click', function () {
-
-        var bg = $(this).css('background-image');
-        bg = bg.replace('url(','').replace(')','').replace(/\"/gi, "");
-        alert(bg);
-    });
+        login(credits);
+    };
 
 
+    /**
+     *  ! Events !
+     */
 
-    var login = function (obj) {
-        $.ajax({
-            type: "post",
-            data: obj,
-            url: "https://efigence-camp.herokuapp.com/api/login",
-            error: function(response) {
-                console.log(response.responseText);
-            },
-            success: function(response) {
-                $('#response').html(JSON.stringify(response)).show();
-                console.log(response);
+    var loginName = $('#login'),
+        passwordInput = $('#password'),
+        goButton = $('.button-go'),
+        messageNode = $('#message'),
+        loader = $('#loader');
+
+
+    passwordInput.on('keydown', function (e) {
+
+        if (e.which == 13) {
+
+            e.preventDefault();
+
+            if (passwordInput.valid()) {
+                submitLogin();
+            } else {
+                messageNode.message(passwordInput.msg)
             }
-        });
-    }
+        }
+    });
+
+
+    goButton.on('click', function (e) {
+
+        e.preventDefault();
+
+        if(passwordInput.valid()) {
+            submitLogin();
+        } else {
+            messageNode.message(passwordInput.msg)
+        }
+    });
 });
